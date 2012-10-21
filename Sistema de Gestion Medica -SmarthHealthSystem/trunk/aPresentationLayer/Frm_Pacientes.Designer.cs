49c828837a8b0b5dd35604087f176ef5ae9e59c7
GroupDevelopersInc <dpolanco1@hotmail.com>
2012-10-21 15:56:49 -0700

    Quite el boton que usaba para identificar

--- Sistema de Gestion Medica -SmarthHealthSystem/trunk/aPresentationLayer/Frm_Pacientes.Designer.cs
+++ Sistema de Gestion Medica -SmarthHealthSystem/trunk/aPresentationLayer/Frm_Pacientes.Designer.cs
@@ -59,7 +59,6 @@
             this.lblTelefono = new DevExpress.XtraEditors.LabelControl();
             this.lblDirecciones = new DevExpress.XtraEditors.LabelControl();
             this.grpDatos = new DevExpress.XtraEditors.GroupControl();
-            this.button1 = new System.Windows.Forms.Button();
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtNSS = new System.Windows.Forms.TextBox();
             this.txtIdentificacion = new System.Windows.Forms.TextBox();
@@ -449,7 +448,6 @@
             // 
             // grpDatos
             // 
-            this.grpDatos.Controls.Add(this.button1);
             this.grpDatos.Controls.Add(this.txtEmail);
             this.grpDatos.Controls.Add(this.txtNSS);
             this.grpDatos.Controls.Add(this.txtIdentificacion);
@@ -506,16 +504,6 @@
             this.grpDatos.Size = new System.Drawing.Size(1151, 217);
             this.grpDatos.TabIndex = 5;
             this.grpDatos.Text = "Datos del Paciente";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(447, 182);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 64;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtEmail
             // 
@@ -1386,6 +1374,5 @@
         private System.Windows.Forms.TextBox txtApellidos;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewComboBoxColumn2;
-        private System.Windows.Forms.Button button1;
     }
 }
