b483e56b9110dabef89c38fb5639e5b47a5be19e
fhoepelman <hoepelman03@hotmail.com>
2012-10-24 06:06:59 -0700

    creacion Bl Historial Clinico

---
+++ Sistema de Gestion Medica -SmarthHealthSystem/trunk/BussinesLogicLayer/Bl_HistorialClinico.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EntidadesLayer;
+using DataAccessLayer;
+
+
+namespace BussinesLogicLayer
+{
+   public class Bl_HistorialClinico
+    {
+
+     
+        public static bool Insert(Ent_HistorialClinico entHistorialClinico)
+        {
+            bool flag = false;
+
+            //Validaciones
+
+
+            Da_HistorialClinico.Insert(entHistorialClinico);
+            flag = true;
+           
+
+            return flag;
+
+        }
+        public bool Update()
+        {
+
+            bool flag = false;
+
+            return flag;
+
+        }
+        public bool Delete()
+        {
+            bool flag = false;
+
+            return flag;
+
+        }
+        public bool Search()
+        {
+            bool flag = false;
+
+            return flag;
+
+        }
+
+
+    }
+}
