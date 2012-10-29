83d220a0d4581f470d6654559e17c6ecf5075800
fhoepelman <hoepelman03@hotmail.com>
2012-10-28 18:25:04 -0700

    Creacion Entidad HAbitos Farmalogicos

---
+++ Sistema de Gestion Medica -SmarthHealthSystem/trunk/EntidadesLayer/Ent_HabitosFarmalogicos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntidadesLayer
+{
+   public  class Ent_HabitosFarmalogicos
+    {
+
+        public bool AntiHipertensivos { get; set; }
+        public bool AntiPlaquetarios { get; set; }
+        public bool AntiCoagulantes { get; set; }
+        public bool AntiArritmicos { get; set; }
+        public bool Antibioticos { get; set; }
+        public bool AntiInlamatorios { get; set; }
+        public bool Esteroides { get; set; }
+        public bool Otros { get; set; }
+        public string Descripcion { get; set; }
+
+
+    }
+}
