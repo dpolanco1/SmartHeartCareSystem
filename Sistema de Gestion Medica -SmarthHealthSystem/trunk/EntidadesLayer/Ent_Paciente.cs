d5c08efe11e2ae72ae931d2efb51dfb2dc10e09b
GroupDevelopersInc <dpolanco1@hotmail.com>
2012-11-17 10:46:05 -0800

    Prueba

--- Sistema de Gestion Medica -SmarthHealthSystem/trunk/EntidadesLayer/Ent_Paciente.cs
+++ Sistema de Gestion Medica -SmarthHealthSystem/trunk/EntidadesLayer/Ent_Paciente.cs
@@ -13,26 +13,15 @@ namespace EntidadesLayer
 		 public  string Apellidos {get;set;}
 		 public  int IDTipoIdentifacion {get;set;}
 		 public  string Identificacion {get;set;}
-		 public  DateTime FechaNacimiento {get;set;}
-		 public  int IDNivelAcademico {get;set;}
-         public int IDOcupacion { get; set; }
-		 public  string Genero {get;set;}
-		 public  string EstadoCivil {get;set;}
-         public string TipoSangre { get; set; }
+         public string Genero { get; set; }
          public int TipoPaciente { get; set; }
-         public string NSS { get; set; }
-         public string Email { get; set; }
-		 public  int IDNacionalidad {get;set;}
-		 public  int IDPais {get;set;}
-         public int IDRegion { get; set; }
-		 public  int  IDProvincia {get;set;}
-		 public  int IDSector {get;set;}
-		 public  DateTime FechaIngrero {get;set;}
+         public string EstadoCivil { get; set; }
+         public string TipoSangre { get; set; }
          public decimal Peso { get; set; }
          public decimal Altura { get; set; }
-         public bool Activo { get; set; }
-         public bool EnvioEmail { get; set; }
-         public string Observaciones { get; set; }
+         public  DateTime FechaNacimiento {get;set;}
+         public string Email { get; set; }
+         public string Direccion { get; set; }
 
 
 
